efb400d4c4354a5eefebfbc5e89463a6a19b63ae
Lucas <lucasprepara12@gmail.com>
2025-05-13 13:10:50 -0300

    Moves the page from app to app/(app) and creates a layout

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,16 @@
+import { Header } from "@/components/layout/header";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const MainLayout = ({ children }: Props) => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex flex-1 flex-col">{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ app/(app)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section className="flex flex-col h-dvh items-center justify-center">
+      <h1>Hello World!</h1>
+    </section>
+  );
+}
