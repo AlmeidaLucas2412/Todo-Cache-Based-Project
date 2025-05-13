2dac45d3fd8c9970fb20d894bb6338817b4c6897
Lucas <lucasprepara12@gmail.com>
2025-05-13 15:19:32 -0300

    Minor improvement

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -8,7 +8,7 @@ const MainLayout = ({ children }: Props) => {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex flex-1 flex-col pt-20">{children}</main>
+      <main className="flex flex-1 flex-col">{children}</main>
     </div>
   );
 };
